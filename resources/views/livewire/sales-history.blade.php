8e04120aa0adc16d25aeb7663ce37d8815eae29b
BroakenMedia <joe@broakenmedia.co.uk>
2024-02-01 22:11:15 +0000

    Update sales history table to reflect additional columns.

--- resources/views/livewire/sales-history.blade.php
+++ resources/views/livewire/sales-history.blade.php
@@ -8,21 +8,24 @@ use Akaunting\Money\Money;
                 <tr class="border-b border-gray-200">
                     <th scope="col" class="px-4 py-3 font-semibold text-xl text-left" colspan="3">Past Sales</th>
                 </tr>
-                <tr>
+                <tr class="bg-gray-200">
+                    <th scope="col" class="t-head">Bean Type</th>
                     <th scope="col" class="t-head">Quantity</th>
                     <th scope="col" class="t-head">Unit Cost</th>
                     <th scope="col" class="t-head">Selling Price</th>
+                    <th scope="col" class="t-head">Sold At</th>
                 </tr>
             </thead>
             <tbody class="divide-y divide-gray-200 ">
                 @foreach($sales as $sale)
                 <tr class="hover:bg-gray-100">
+                    <td class="px-4 py-2">{{ $sale->product->name }}</td>
                     <td class="px-4 py-2">{{ $sale->quantity }}</td>
                     <td class="px-4 py-2">{{ Money::GBP($sale->unit_cost, true) }}</td>
                     <td class="px-4 py-2">{{ Money::GBP($sale->sale_price, true) }}</td>
+                    <td class="px-4 py-2">{{ $sale->created_at }}</td>
                 </tr>
                 @endforeach
-
             </tbody>
         </table>
         <div class="px-4 py-2 border-t">
